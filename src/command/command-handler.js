b7bc6b35ab2d4a24235b11029e662557b7661509
huda0209 <huda030209@gmail.com>
2022-01-27 00:30:23 +0900

    [chg]ignoreChannelCommandRunner.jsへ改名に伴い定数名変更

--- src/command/command-handler.js
+++ src/command/command-handler.js
@@ -15,7 +15,7 @@ const logger = require('../util/logger');
 const admin = require('./admin/admin.js');
 const embedContent = require("../util/embed");
 
-const ignoreChannelManager = require("./ignoreChannelManager");
+const ignoreChannelCommandRunner = require("./ignoreChannelCommandRunner");
 
 
 /*
@@ -39,7 +39,7 @@ module.exports = (
         console.log(content)
         console.log(content.split('#'));
 */
-
+        console.log(message.guild.channels.cache)
         if(!message.content.startsWith(configManager.getBotData("PREFIX"))) return;
 
         const [command, ...args] = message.content.slice(configManager.getBotData("PREFIX").length).split(' ');   
@@ -71,8 +71,29 @@ async function AdminCommandHandler([command, ...args],message,client){
             process.exit(0);
 
         case "ignorechannel":
-            ignoreChannelManager([command, ...args],message);
+            ignoreChannelCommandRunner([command, ...args],message);
             break;
+
+        case "ignorecategory":
+            if(args.length<2){
+                message.reply(embedContent.errorWithTitle(`❌**コマンド実行失敗**❌`, `引数が不足しています。\n実行例\`${configManager.getBotData("PREFIX")}${configManager.getBotData("COMMAND")} ignoreChannel <add | remove> [チャンネル...]\``))
+                return;
+            }
+            switch(args[1].toLowerCase()){
+                case "add":
+
+                    break;
+
+
+
+
+
+                case "remove":
+
+                    break;
+
+
+            }
 
         case "help" :
             message.channel.send({embeds:[embedContent.info(`**移行するメッセージのあるチャンネル(若しくはスレッド)で下記のコマンドを入力**\n・チャンネルorスレッド → チャンネル\n\`${configManager.getBotData("PREFIX")}${configManager.getBotData("COMMAND")} run <移行する最初のメッセージのid> <移行先のチャンネルid> <移行するメッセージ数>\`\n\n・チャンネルorスレッド → スレッド\n\`${configManager.getBotData("PREFIX")}${configManager.getBotData("COMMAND")} run <移行する最初のメッセージのid> <移行先のスレッドがあるチャンネルid>:<移行先のスレッドid> <移行するメッセージ数>\``)]})
