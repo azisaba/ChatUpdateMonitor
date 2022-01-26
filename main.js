bfc419893d6efcba5f80519fd5453110d99171b2
huda0209 <huda030209@gmail.com>
2022-01-26 23:36:50 +0900

    [chg]外部モジュール化のためコード変更 configManager導入に伴いconfigの読み込みコード削除 configで読み込んでいた部分をconfigManager経由の読み込みコードに変更 messageCreateListener.jsとcommand-handler.jsのモジュール読み込みコード追加 readyイベントの引数を削除 イベント受取部分のソースファイル分離に伴いmessageCreateイベントのコード削除

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ main.js
  
 ran by node.js
 
-2022-1-24
+2022-1-26
 
 */
 'use strict'
@@ -18,12 +18,13 @@ const discord = require("discord.js");
 require('date-utils');
 
 //module
-const commandHandler = require('./src/command/command-handler.js');
+//const commandHandler = require('./src/command/command-handler.js');
 
 //other 
 const client = new discord.Client({intents: ["GUILDS", "GUILD_MESSAGES","DIRECT_MESSAGES"], partials: ["USER", "MESSAGE", "CHANNEL"]});
 const logger = require('./src/util/logFile');
 const config = require('./src/util/config');
+const configManager = require("./src/config/configManager");
 const Package = require("./package.json");
 
 logger.info(`This service is standing now...`);
@@ -37,39 +38,29 @@ process.on("SIGINT", ()=>{
     process.exit(0);
 });
 
-//config
-config.exist(true);
-const BOT_DATA = config.loadConfig("setting.json");
-let guildData = config.loadConfig("guildData.json");
 
+require("./src/listener/messageCreateListener")(client);
+require("./src/command/command-handler")(client);
 
 //start the bot
-client.on("ready", message => {
+client.on("ready", () => {
 	logger.info(`bot is ready! ver. ${Package.version} \n        login: {cyan}${client.user.tag}{reset}\n`);
-  	client.user.setActivity(`${BOT_DATA.PREFIX}${BOT_DATA.COMMAND} helpでヘルプを表示 ver. ${Package.version}`, { type: 'PLAYING' });
+  	client.user.setActivity(`${configManager.getBotData("PREFIX")}${configManager.getBotData("COMMAND")} helpでヘルプを表示 ver. ${Package.version}`, { type: 'PLAYING' });
 });
 
-//message event
-client.on("messageCreate", async message => {
-  	if (message.content.startsWith(BOT_DATA.PREFIX)){
-    	const [command, ...args] = message.content.slice(BOT_DATA.PREFIX.length).split(' ');   
-      	commandHandler.commandHandler([command, ...args],message,guildData,BOT_DATA,client)
-  	};
-})
 
 let token;
 if(process.argv.length == 3){
   	switch(process.argv[2]){
   	  	case "main" :
-  	    	token = BOT_DATA.MAIN_TOKEN;
+  	    	token = configManager.getBotData("MAIN_TOKEN");
   	    	break;
   	  	case "dev" :
-  	    	if(!BOT_DATA.DIV_TOKEN){
+  	    	if(!configManager.getBotData("DEV_TOKEN")){
 				logger.error(`Don't have a property "{red}DIV_TOKEN{reset}" in {green}setting.json{reset}.`);
 				process.exit(0);
 			}
-  	    	token = BOT_DATA.DIV_TOKEN;
-  	    	BOT_DATA.VERSION = `dev(${BOT_DATA.VERSION})`;
+  	    	token = configManager.getBotData("DEV_TOKEN");
 			require("./src/util/checkVersion");
   	    	break;
   	  	default :
@@ -77,7 +68,7 @@ if(process.argv.length == 3){
   	    	process.exit(0);
   	};
 }else if(process.argv.length == 2){
-	token = BOT_DATA.MAIN_TOKEN;
+	token = configManager.getBotData("MAIN_TOKEN");
 }else{
 	logger.error(`Unknown command. \nUsage \n {green}node main.js main{reset} : use main token \n {green}node main.js div{reset} : use divelopment token`);
 	process.exit(0);
