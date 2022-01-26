31559bb21e5603819b8dc2378dbd525180e72966
huda0209 <huda030209@gmail.com>
2022-01-26 20:49:17 +0900

    [add]メッセージの更新を監視し、タイムスタンプをアップデートするコード追加

---
+++ src/listener/messageCreateListener.js
@@ -0,0 +1,24 @@
+/*
+const discord = require("discord.js")
+const client = new discord.Client({intents: ["GUILDS", "GUILD_MESSAGES","DIRECT_MESSAGES"], partials: ["USER", "MESSAGE", "CHANNEL"]});
+//*/
+
+const configManager = require("../config/configManager");
+
+module.exports = (
+    //*
+    client
+    //*/
+    )=>{
+      client.on("messageCreate", async message => {
+      
+      const parentId = message.channel.parentId;
+      const channelId = message.channelId;
+      
+      if(configManager.existIgnoreCategory(parentId)|| configManager.existIgnoreChannel(channelId)) return;
+
+      const createdAt = message.createdAt;
+      configManager.setCategoryLastUpdate(parentId, createdAt);
+  })
+}
+
