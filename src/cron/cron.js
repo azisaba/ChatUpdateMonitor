a1cf51cd0a37ab456224209e5ec3307dc8ca6ed5
huda0209 <huda030209@gmail.com>
2022-01-27 14:09:53 +0900

    [add]周期的に経過時間を監視するソースコード追加

---
+++ src/cron/cron.js
@@ -0,0 +1,23 @@
+
+
+
+//const cron = require("node-cron");
+const embedContent = require("../util/embed");
+const configManager = require("../config/configManager");
+
+module.exports = async (client)=>{
+    setInterval( async () => {
+        configManager.getMonitorCategoryList().forEach(async key=>{
+            if(configManager.getCategoryLastUpdate(key) > (new Date()).getTime()) return;
+            if(configManager.getNotifyStatus(key)) return;
+            try{
+                (await client.channels.fetch("882261188413620274")).send(
+                    embedContent.infoWithTitle(`最後のアクションから${10}経ちました!`, `対象カテゴリ: ${(await client.channels.fetch(key)).name}\n最終アクション<t:${Math.floor(configManager.getCategoryLastUpdate(key)/1000)}:F><t:${Math.floor(configManager.getCategoryLastUpdate(key)/1000)}:R>`
+                    ))
+                configManager.setNotifyStatus(key, true);
+            }catch(e){ 
+                //console.log(e)
+            }
+        })
+    }, 1000)
+}
