c3555a08d7e1d4c2421f5268dc5cfb7d59d0ddc5
huda0209 <huda030209@gmail.com>
2022-01-28 01:45:50 +0900

    [add]監視期間から外れたカテゴリに新たなメッセージが投稿されると通知するコード追加

--- src/listener/messageCreateListener.js
+++ src/listener/messageCreateListener.js
@@ -12,20 +12,26 @@ ran by node.js
 "use strict"
 
 const configManager = require("../config/configManager");
+const embedContent = require("../util/embed");
 
 module.exports = (client)=>{
-      client.on("messageCreate", async message => {
-      
-      const parentId = message.channel.parentId;
-      const channelId = message.channelId;
-      
-      if(configManager.existIgnoreCategory(parentId)
-          || configManager.existIgnoreChannel(channelId)
-          || message.author.bot
-          || message.content.startsWith(configManager.getBotData("PREFIX"))
-        ) return;
-
-      const createdAt = message.createdTimestamp;
-      configManager.setCategoryLastUpdate(parentId, createdAt);
+    client.on("messageCreate", async message => {
+    
+    const parentId = message.channel.parentId;
+    const channelId = message.channelId;
+    
+    if(configManager.existIgnoreCategory(parentId)
+        || configManager.existIgnoreChannel(channelId)
+        || message.author.bot
+        || message.content.startsWith(configManager.getBotData("PREFIX"))
+      ) return;
+    
+    if(configManager.existMonitorCategory(parentId) && configManager.getNotifyStatus(parentId)){
+        message.guild.channels.cache.get(configManager.getGuildtData("sendSystemMessageChannelId")).send(
+			embedContent.infoWithTitle(`📈新たなアクションを検出しました!`, `対象カテゴリ: ${(await client.channels.fetch(parentId)).name}\n前回のアクション : <t:${Math.floor(configManager.getCategoryLastUpdate(parentId)/1000)}:F><t:${Math.floor(configManager.getCategoryLastUpdate(parentId)/1000)}:R>`
+		));
+    }
+    const createdAt = message.createdTimestamp;
+    configManager.setCategoryLastUpdate(parentId, createdAt);
   })
 }
