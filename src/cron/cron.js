76326d207e39880798e2efbae1e0ea74567de984
huda0209 <huda030209@gmail.com>
2022-01-27 22:42:51 +0900

    [add]指定された期間アクションがないか判定するコード追加

--- src/cron/cron.js
+++ src/cron/cron.js
@@ -1,22 +1,32 @@
 
 
 
-//const cron = require("node-cron");
 const embedContent = require("../util/embed");
 const configManager = require("../config/configManager");
 
 module.exports = async (client)=>{
     setInterval( async () => {
+        const period = configManager.getGuildtData("period");
+
+        const timeD = Math.floor(period / (24 * 60 * 60));
+        const timeH = Math.floor(period % (24 * 60 * 60) / (60 * 60));
+        const timeM = Math.floor(period % (24 * 60 * 60) % (60 * 60) / 60);
+        const timeS = period % (24 * 60 * 60) % (60 * 60) % 60;
+        const timeString = `${timeD>0?`${timeD}日` : ""}${timeH>0?`${timeH}時間` : ""}${timeM>0?`${timeM}分` : ""}${timeS>0?`${timeS}秒` : ""}`
+
         configManager.getMonitorCategoryList().forEach(async key=>{
-            if(configManager.getCategoryLastUpdate(key) > (new Date()).getTime()) return;
+            const lastUpdateDate = (new Date(configManager.getCategoryLastUpdate(key)));
+            lastUpdateDate.setSeconds(lastUpdateDate.getSeconds()+period);
+
+            if(lastUpdateDate.getTime() > (new Date()).getTime()) return;
             if(configManager.getNotifyStatus(key)) return;
             try{
                 (await client.channels.fetch(configManager.getGuildtData("sendSystemMessageChannelId"))).send(
-                    embedContent.infoWithTitle(`最後のアクションから${10}経ちました!`, `対象カテゴリ: ${(await client.channels.fetch(key)).name}\n最終アクション : <t:${Math.floor(configManager.getCategoryLastUpdate(key)/1000)}:F><t:${Math.floor(configManager.getCategoryLastUpdate(key)/1000)}:R>`
+                    embedContent.infoWithTitle(`最後のアクションから${timeString}経ちました!`, `対象カテゴリ: ${(await client.channels.fetch(key)).name}\n最終アクション : <t:${Math.floor(configManager.getCategoryLastUpdate(key)/1000)}:F><t:${Math.floor(configManager.getCategoryLastUpdate(key)/1000)}:R>`
                     ))
                 configManager.setNotifyStatus(key, true);
             }catch(e){ 
-                //console.log(e)
+                console.log(e)
             }
         })
     }, 1000)
